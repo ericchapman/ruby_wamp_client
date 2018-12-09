8afba725bb0718737390d6eeeb43114db850e982
Eric Chapman <eric.chappy@gmail.com>
2018-12-09 14:42:46 -0600

    added comments to event module

--- lib/wamp/client/event.rb
+++ lib/wamp/client/event.rb
@@ -1,5 +1,33 @@
 module Wamp
   module Client
+
+    # Module that adds event capabilities to the class.
+    #
+    # Usage:
+    #
+    #   class MyClass
+    #     include Event
+    #
+    #     create_event [:open, :close]
+    #
+    #     def do_something
+    #       trigger :open, 4
+    #     end
+    #
+    #   end
+    #
+    #   object = MyClass.new
+    #
+    #   object.on(:open) do |value|
+    #     puts value
+    #   end
+    #
+    #   object.do_something
+    #
+    # Prints:
+    #
+    #   4
+    #
     module Event
 
       def self.included(base)
@@ -11,15 +39,17 @@ module Wamp
           attribute ||= :event_callbacks
           setter ||= :on
           trigger ||= :trigger
-
-          attr_accessor attribute
-
           event_name = "#{attribute}_events"
 
+          # Creates the attribute to store the callbacks
+          attr_accessor attribute
+
+          # Creates the attributes to store the allowed events
           define_method event_name do
             events
           end
 
+          # Creates the setter.  Default: "on"
           define_method setter do |event, &handler|
             unless self.send(event_name).include?(event)
               raise RuntimeError, "unknown #{setter}(event) '#{event}'"
@@ -30,6 +60,7 @@ module Wamp
             self.send("#{attribute}=", callback)
           end
 
+          # Create the trigger.  Default: "trigger"
           define_method trigger do |event, *args|
             handler = (self.send(attribute) || {})[event]
             if handler != nil
