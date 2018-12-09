3b1d5522438b5de68940d109223ae6b9632964e9
Eric Chapman <eric.chappy@gmail.com>
2018-12-08 19:49:33 -0600

    added response parsing methods

--- lib/wamp/client/response.rb
+++ lib/wamp/client/response.rb
@@ -1,6 +1,7 @@
 module Wamp
   module Client
     module Response
+      DEFAULT_ERROR = "wamp.error.runtime"
 
       # This method wraps the handling of the result from a procedure.
       # or interrupt.  It is intended to standardize the processing
@@ -14,7 +15,7 @@ module Wamp
         rescue CallError => e
           result = e
         rescue StandardError => e
-          result = CallError.new('wamp.error.runtime', [e.to_s], { backtrace: e.backtrace })
+          result = CallError.new(DEFAULT_ERROR, [e.to_s], { backtrace: e.backtrace })
         end
 
         # Ensure an expected class is returned
@@ -25,12 +26,29 @@ module Wamp
         end
       end
 
+      # This method will instantiate either a CallResult or CallError based
+      # on the payload
+      #
+      # @param hash [Hash] - The hash
+      # @return [CallResult, CallError] - The result
+      def self.from_hash(hash)
+        if hash[:error] != nil
+          CallError.from_hash(hash)
+        else
+          CallResult.from_hash(hash)
+        end
+      end
+
       class CallResult
         attr_reader :args, :kwargs
 
         def initialize(args=nil, kwargs=nil)
           @args = args || []
           @kwargs = kwargs || {}
+        end
+
+        def self.from_hash(hash)
+          self.new(hash[:args], hash[:kwargs])
         end
 
         def to_hash
@@ -52,7 +70,7 @@ module Wamp
         end
       end
 
-      class CallError < Exception
+      class CallError < StandardError
         attr_reader :error, :args, :kwargs
 
         def initialize(error, args=nil, kwargs=nil)
@@ -61,8 +79,12 @@ module Wamp
           @kwargs = kwargs || {}
         end
 
+        def self.from_hash(hash)
+          self.new(hash[:error], hash[:args], hash[:kwargs])
+        end
+
         def to_hash
-          { error: error, args: self.args, kwargs: self.kwargs }
+          { error: self.error, args: self.args, kwargs: self.kwargs }
         end
 
         def self.from_message(msg)
@@ -74,7 +96,7 @@ module Wamp
             result
           else
             args = result != nil ? [result] : nil
-            self.new("wamp.error.runtime", args)
+            self.new(DEFAULT_ERROR, args)
           end
         end
       end
